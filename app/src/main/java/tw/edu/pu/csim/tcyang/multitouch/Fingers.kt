ac78b491e8a46891f10e09e2ab9cff71b3b547a6
tcyang <tcyang@gm.pu.edu.tw>
2021-03-27 11:07:52 +0800

    觸控依序呈現彩虹顏色

--- app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
@@ -7,7 +7,7 @@ import android.graphics.Paint
 import android.view.MotionEvent
 import android.view.View
 
-class Fingers(context: Context?) : View(context) {
+class Fingers(context: Context) : View(context) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     //var xPos:Float = 200f
     //var yPos:Float = 200f
@@ -16,15 +16,26 @@ class Fingers(context: Context?) : View(context) {
     var xPos = FloatArray(20)
     var yPos = FloatArray(20)
 
+    var rainbow = IntArray(7)
+
+    init {
+        rainbow = context.getResources().getIntArray(R.array.rainbow)
+    }
+
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawColor(Color.LTGRAY)
         paint.color = Color.YELLOW
 
         for (i in 0..Count-1) {
-            paint.color = Color.YELLOW
+            //paint.color = Color.YELLOW
+            paint.color =rainbow[i % 7]
             canvas.drawCircle(xPos[i], yPos[i], 80f, paint)
         }
+        paint.color = Color.BLUE
+        paint.textSize = 50f
+        canvas.drawText("多指觸控，圓形呈現彩虹顏色！", 50f,200f, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean{
