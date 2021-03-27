7cb7605506143af1f6e0b6e3208511e304e8ee04
tcyang <tcyang@gm.pu.edu.tw>
2021-03-27 11:22:09 +0800

    縮放圖片

--- app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
@@ -3,9 +3,12 @@ package tw.edu.pu.csim.tcyang.multitouch
 import android.content.Context
 import android.graphics.*
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 
-class Fingers(context: Context) : View(context) {
+class Fingers(context: Context) : View(context),
+    ScaleGestureDetector.OnScaleGestureListener{
+
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     //var xPos:Float = 200f
     //var yPos:Float = 200f
@@ -16,10 +19,13 @@ class Fingers(context: Context) : View(context) {
 
     var rainbow = IntArray(7)
     lateinit var bitmap: Bitmap
+    lateinit var sg: ScaleGestureDetector
+    var factor: Float = 1.0f
 
     init {
         rainbow = context.getResources().getIntArray(R.array.rainbow)
         bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.robot)
+        sg = ScaleGestureDetector(context, this)
     }
 
 
@@ -38,8 +44,10 @@ class Fingers(context: Context) : View(context) {
         canvas.drawText("多指觸控，圓形呈現彩虹顏色！", 50f,200f, paint)
 
         var SrcRect:Rect = Rect(0, 0, bitmap.width, bitmap.height) //裁切(顯示全部)
+        var w:Int = (bitmap.width*factor/4).toInt()
+        var h:Int = (bitmap.height*factor/4).toInt()
         var DestRect:Rect = Rect(200, 300,
-            bitmap.width/4+200, bitmap.height/4+300) //原圖較大，縮成1/4顯示
+            w+200, h+300) //原圖較大，縮成1/4顯示
         canvas.drawBitmap(bitmap, SrcRect, DestRect, paint)
     }
 
@@ -54,7 +62,21 @@ class Fingers(context: Context) : View(context) {
         }
 
         invalidate()
+        sg.onTouchEvent(event)
         return true
+    }
+
+    override fun onScale(p0: ScaleGestureDetector?): Boolean {
+        factor += sg.getScaleFactor()-1
+        return true
+    }
+
+    override fun onScaleBegin(p0: ScaleGestureDetector?): Boolean {
+        return true
+    }
+
+    override fun onScaleEnd(p0: ScaleGestureDetector?) {
+
     }
 
 
