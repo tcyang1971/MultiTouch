48fbdd77fd1475ec03f704c2151aabc30f73b5f7
tcyang <tcyang@gm.pu.edu.tw>
2021-03-27 10:53:51 +0800

    處理多點觸控

--- app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/multitouch/Fingers.kt
@@ -9,19 +9,34 @@ import android.view.View
 
 class Fingers(context: Context?) : View(context) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-    var xPos:Float = 200f
-    var yPos:Float = 200f
+    //var xPos:Float = 200f
+    //var yPos:Float = 200f
+
+    var Count:Int = 0;
+    var xPos = FloatArray(20)
+    var yPos = FloatArray(20)
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawColor(Color.LTGRAY)
         paint.color = Color.YELLOW
-        canvas.drawCircle(xPos, yPos, 80f, paint)
+
+        for (i in 0..Count-1) {
+            paint.color = Color.YELLOW
+            canvas.drawCircle(xPos[i], yPos[i], 80f, paint)
+        }
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean{
-        xPos = event.getX()
-        yPos = event.getY()
+        //xPos = event.getX()
+        //yPos = event.getY()
+
+        Count = event.getPointerCount()
+        for (i in 0..Count-1) {
+            xPos[i] = event.getX(i)
+            yPos[i] = event.getY(i)
+        }
+
         invalidate()
         return true
     }
